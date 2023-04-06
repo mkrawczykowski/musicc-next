40c25c1d661bd91939ac31561a749e2d5cd2c96a
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-04-06 15:07:39 +0200

    basic next.js with cookieconsent & GTM working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
+import 'vanilla-cookieconsent/dist/cookieconsent.css';
+import CookieConsent from '../components/structure/CookieConsent/CookieConsent'
 import type { AppProps } from 'next/app'
+import GTM from '../components/structure/GTM/GTM'
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <CookieConsent/>
+    
+    
+    <Component {...pageProps} />
+    </>
+  )
+  
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <script type="text/plain" data-cookiecategory="analytics_storage"
+    dangerouslySetInnerHTML={{
+      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-T4TSM6F');`,
+    }}
+  />
+      </Head>
       <body>
         <Main />
         <NextScript />
