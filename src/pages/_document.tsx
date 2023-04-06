b1af39a69847791aac2fba8b0799780d8bfb2c8d
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-04-06 15:47:53 +0200

    added ID to Script with inline code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <Script type="text/plain" data-cookiecategory="analytics_storage"
+      <Script id="gtm-code" type="text/plain" data-cookiecategory="analytics_storage"
     dangerouslySetInnerHTML={{
       __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
