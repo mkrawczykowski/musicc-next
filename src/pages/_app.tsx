96b6b31cdb9ddbf1d43d784f97f43a064f0a076d
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-04-12 21:02:33 +0200

    added Layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import '@/styles/globals.css'
 import 'vanilla-cookieconsent/dist/cookieconsent.css';
 import type {AppProps} from 'next/app';
-import CookieConsent from '../components/structure/CookieConsent/CookieConsent'
+import Layout from '../components/structure/Layout/Layout';
+import CookieConsent from '../components/structure/CookieConsent/CookieConsent';
 import Store from '../components/structure/Store/Store';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Store>
-      <CookieConsent/>
-      <Component {...pageProps} />
+      <Layout>
+        <CookieConsent/>
+        <Component {...pageProps} />  
+      </Layout>
     </Store>
   )
   
