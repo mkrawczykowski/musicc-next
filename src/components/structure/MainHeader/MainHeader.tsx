01114985bfc877d24898479411f8da40afaff654
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-04-18 17:44:02 +0200

    added Grid

--- src/components/structure/MainHeader/MainHeader.tsx
+++ src/components/structure/MainHeader/MainHeader.tsx
@@ -1,10 +1,17 @@
 import react from 'react';
 import styles from './MainHeader.module.scss';
+import {Col, Container, Row} from '../Grid/Grid';
 
 const MainHeader = () => {
     return (
         <header className={styles.mainHeader}>
-           <h1>head</h1>
+            <Container>
+                <Row>
+                    <Col classes="col-lg-6">
+                        <h1>head</h1>
+                    </Col>
+                </Row>
+            </Container>
         </header>
         
     )
