346b9f4367b1447075a4c76c4ab97ef143633a06
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-04-18 17:16:37 +0200

    sass working

--- src/components/structure/MainHeader/MainHeader.tsx
+++ src/components/structure/MainHeader/MainHeader.tsx
@@ -1,8 +1,9 @@
 import react from 'react';
+import styles from './MainHeader.module.scss';
 
 const MainHeader = () => {
     return (
-        <header>
+        <header className={styles.mainHeader}>
            <h1>head</h1>
         </header>
         
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 import 'vanilla-cookieconsent/dist/cookieconsent.css';
 import type {AppProps} from 'next/app';
 import Layout from '../components/structure/Layout/Layout';
