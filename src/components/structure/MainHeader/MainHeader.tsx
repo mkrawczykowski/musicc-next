4fe03ce9db635ecf98b15a3b6d85e40c1420c5bb
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-04-18 08:59:13 +0200

    MainHeader

---
+++ src/components/structure/MainHeader/MainHeader.tsx
@@ -0,0 +1,9 @@
+import react from 'react';
+
+const MainHeader = () => {
+    return (
+        <p>head</p>
+    )
+}
+
+export default MainHeader;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,15 +5,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      {/* <Script id="gtm-code" type="text/plain" data-cookiecategory="analytics_storage"
-    dangerouslySetInnerHTML={{
-      __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-T4TSM6F');`,
-    }}
-  /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
