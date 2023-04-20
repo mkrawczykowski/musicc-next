afdf5ed59d1fa881bd35097a631184ef962cc461
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-04-20 12:20:51 +0200

    added children to Column

--- src/components/structure/Grid/Grid.tsx
+++ src/components/structure/Grid/Grid.tsx
@@ -1,60 +1,59 @@
 import React from 'react';
 import styles from './Grid.module.scss';
 
-const Container = ({children}) => {
-    return(
+const Container = ({ children }) => {
+    return (
         <div className={styles.container}>
             {children}
         </div>
     )
 }
 
-const Row = ({children}) => {
-    return(
+const Row = ({ children }) => {
+    return (
         <div className={styles.row}>
             {children}
         </div>
     )
 }
 
-const Col = ({children, classes}) => {
-    console.log(classes);
+const Col = ({ children, classes, className }) => {
     const splitClasses = classes.split(' ');
-    console.log(splitClasses)
 
-        const reactifySplitClass = (classToReactify) => {
-            
-            let charPosition = 0;
-            let dashPosition = 0;
-            let uppercasedClass = '';
-            let removedDashClass = '';
+    const reactifySplitClass = (classToReactify) => {
 
-            while (charPosition < classToReactify.length && dashPosition >= 0){
-                dashPosition = classToReactify.indexOf('-', charPosition);
+        let charPosition = 0;
+        let dashPosition = 0;
+        let uppercasedClass = '';
+        let removedDashClass = '';
 
-                const replaceAt = (whereToReplace, whatPosition, newValue) => {
-                    return whereToReplace.substring(0, whatPosition) + newValue + whereToReplace.substring(whatPosition+1);
-                }
+        while (charPosition < classToReactify.length && dashPosition >= 0) {
+            dashPosition = classToReactify.indexOf('-', charPosition);
 
-                if (dashPosition > 0){
-                    uppercasedClass = replaceAt(classToReactify, dashPosition+1, classToReactify[dashPosition+1].toUpperCase());
-                    removedDashClass = replaceAt(uppercasedClass, dashPosition, '');
-                }
-
-                charPosition = dashPosition + 1;
-                classToReactify = removedDashClass;
+            const replaceAt = (whereToReplace, whatPosition, newValue) => {
+                return whereToReplace.substring(0, whatPosition) + newValue + whereToReplace.substring(whatPosition + 1);
             }
-            return styles[removedDashClass];
+
+            if (dashPosition > 0) {
+                uppercasedClass = replaceAt(classToReactify, dashPosition + 1, classToReactify[dashPosition + 1].toUpperCase());
+                removedDashClass = replaceAt(uppercasedClass, dashPosition, '');
+            }
+
+            charPosition = dashPosition + 1;
+            classToReactify = removedDashClass;
         }
+        return styles[removedDashClass];
+    }
 
     let reactifiedClasses = '';
     splitClasses.map((splitClass) => {
         reactifiedClasses += styles[splitClass];
         reactifiedClasses += ' ';
     })
-    
-    return(
-        <div className={[styles.col, reactifiedClasses].join(' ')}>
+    console.log(className);
+
+    return (
+        <div className={[styles.col, reactifiedClasses, className].join(' ')}>
             {children}
         </div>
     )
@@ -64,4 +63,4 @@ Col.defaultProps = {
     classes: '',
 }
 
-export {Container, Row, Col};
+export { Container, Row, Col };
--- src/components/structure/MainHeader/MainHeader.tsx
+++ src/components/structure/MainHeader/MainHeader.tsx
@@ -1,6 +1,6 @@
 import react from 'react';
 import styles from './MainHeader.module.scss';
-import {Container, Col, Row} from '../Grid/Grid';
+import { Container, Col, Row } from '../Grid/Grid';
 import Logo from '../Logo/Logo';
 import Search from '../Search/Search';
 
@@ -9,16 +9,14 @@ const MainHeader = () => {
         <header className={styles.mainHeader}>
             <Container>
                 <Row>
-                    <Col classes="col-xs-12 col-lg-4">
-                        <Logo/>
-                    </Col>
-                    <Col classes="col-xs-12 col-lg-4">
+                    <Col classes="col-xs-12 col-lg-4" className={styles.mainHeader__structure}>
+                        <Logo />
+
                         <Search />
                     </Col>
                 </Row>
             </Container>
         </header>
-        
     )
 }
 
