fabba4818ae60a5240d863fe156481ae7b1d6c7f
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-05-01 08:44:18 +0200

    autoformatting

--- src/components/structure/Grid/Grid.tsx
+++ src/components/structure/Grid/Grid.tsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import styles from './Grid.module.scss';
 
-const Container = ({children}) => {
-    return(
+const Container = ({ children }) => {
+    return (
         <div className={styles.container}>
             {children}
         </div>
     )
 }
 
-const Row = ({children}) => {
-    return(
+const Row = ({ children }) => {
+    return (
         <div className={styles.row}>
             {children}
         </div>
     )
 }
 
-const Col = ({children, classes}) => {
-    const splitClasses = classes.split(' ');
+const Col = ({ children, columnClasses, className }) => {
+    const splitClasses = columnClasses.split(' ');
 
     let separatedClasses = '';
     splitClasses.map((splitClass) => {
         separatedClasses += styles[splitClass];
         separatedClasses += ' ';
     })
-    
-    return(
+
+    return (
         <div className={[styles.col, separatedClasses].join(' ')}>
             {children}
         </div>
@@ -37,4 +37,4 @@ Col.defaultProps = {
     classes: '',
 }
 
-export {Container, Row, Col};
+export { Container, Row, Col };
