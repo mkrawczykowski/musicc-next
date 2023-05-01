c0f2fd58e1f2eb907413e8a2895ff59753bb92d9
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-05-01 19:52:40 +0200

    TracksWithFilters & TaxonomyFilterList & change in typography

--- src/components/content/TracksWithFilters/TracksWithFilters.tsx
+++ src/components/content/TracksWithFilters/TracksWithFilters.tsx
@@ -1,21 +1,46 @@
 import React from "react";
 import styles from './TracksWithFilters.module.scss';
 import { Container, Col } from '../../structure/Grid/Grid';
+import TaxonomyFilterList from '../TaxonomyFilterList/TaxonomyFilterList';
 
-function TracksWithFilters() {
-  console.log(styles);
+const moods = [
+  'warm',
+  'cold',
+  'mysterious',
+  'emotional',
+  'happy',
+  'sad',
+  'melancholic',
+  'romantic',
+  'calm',
+  'energeting/action',
+  'motivation',
+  'upbeat',
+]
+
+const instruments = [
+  'synth',
+  'piano',
+  'drums',
+  'bass',
+  'double bass',
+];
+
+const TracksWithFilters = () => {
   return (
     <section className={styles.tracksWithFilters}>
       <Container classes={'containerTwoColumns'}>
+        <h2 className={styles.tracksWithFilters__sectionHeading}>Free music</h2>
+        <h3 className={styles.tracksWithFilters__sectionSubheading}>(Creative Commons)</h3>
         <Col>
-          ssdsdsds
+          <TaxonomyFilterList title="by mood" taxonomies={moods}></TaxonomyFilterList>
+          <TaxonomyFilterList title="by instrument" taxonomies={instruments}></TaxonomyFilterList>
         </Col>
         <Col>
           dssddssd
         </Col>
       </Container>
     </section>
-
   );
 }
 
