6a6ccd40881969e7e75aac47f73cfb4f45f0f7b3
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-05-07 09:19:03 +0200

    contents in SectionAboutMe

--- src/components/sections/SectionAboutMe/SectionAboutMe.tsx
+++ src/components/sections/SectionAboutMe/SectionAboutMe.tsx
@@ -7,9 +7,16 @@ const SectionAboutMe = () => {
     <section className={styles.sectionAboutMe}>
       <Container classes={'containerTwoColumns'}>
         <Col>
-          <h2 className={styles.sectionAboutMe__header}>Im Michael</h2>
+          <h2 className={styles.sectionAboutMe__heading}>Im Michael</h2>
+          <h3 className={styles.sectionAboutMe__subHeading}>a front end developer from Poland</h3>
+          <p>
+            In my spare time I change my computer keyboard to MIDI keyboard and have fun composing music.
+          </p>
+          <a href="#">Read more</a>
         </Col>
-        <Col></Col>
+        <Col>
+
+        </Col>
       </Container>
     </section>
   )
