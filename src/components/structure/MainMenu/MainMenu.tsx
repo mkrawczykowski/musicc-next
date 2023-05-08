dcc434d0743764a10c0c87750f270fdc417a4b80
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-05-08 21:56:24 +0200

    sub menu structure

--- src/components/structure/MainMenu/MainMenu.tsx
+++ src/components/structure/MainMenu/MainMenu.tsx
@@ -4,9 +4,44 @@ import MainMenuItem from '../MainMenuItem/MainMenuItem';
 
 const MainMenu = ({ }) => {
 
+  const subMenuData =
+  {
+    megaMenuType: 'mega_menu_1',
+    widgetsData: [
+      {
+        widgetType: 'list',
+        widgetData: ['warm', 'cold', 'mysterious', 'emotional', 'happy'],
+        widgetHeading: 'by mood',
+        widgetLinkLabel: 'see all moods',
+        widgetLinkUrl: '#'
+      },
+      {
+        widgetType: 'list',
+        widgetData: ['synth', 'piano', 'drums', 'bass', 'double bass'],
+        widgetHeading: 'by mood',
+        widgetLinkLabel: 'see all moods',
+        widgetLinkUrl: '#'
+      },
+      {
+        widgetType: 'simple',
+        widgetData: ['tracks with the biggest number of downloads'],
+        widgetHeading: 'Most popular tracks',
+        widgetLinkLabel: 'see all tracks',
+        widgetLinkUrl: '#'
+      },
+      {
+        widgetType: 'simple',
+        widgetData: ['tracks I like the most'],
+        widgetHeading: 'FEATURED TRACKS - TRACKS I LIKE THE MOST',
+        widgetLinkLabel: 'see all tracks',
+        widgetLinkUrl: '#'
+      },
+    ]
+  }
+
   return (
     <ul className={styles.MainMenu}>
-      <MainMenuItem label="free music" url="/" />
+      <MainMenuItem label="free music" url="/" subMenuData={subMenuData} />
       <MainMenuItem label="Creative Commons 4.0 license" url="/" />
       <MainMenuItem label="About the author" url="/" />
       <MainMenuItem label="Contact the author" url="/" />
--- src/components/structure/MainMenuItem/MainMenuItem.tsx
+++ src/components/structure/MainMenuItem/MainMenuItem.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Link from 'next/link';
 import MegaMenuPanel from '../MegaMenuPanel/MegaMenuPanel';
 
-const MainMenuItem = ({ label, url }) => {
+interface MainMenuItemProps {
+  label: string,
+  url: string,
+  subMenuData?: any
+}
+
+const MainMenuItem = ({ label, url, subMenuData }: MainMenuItemProps) => {
   return (
     <li>
       <Link href={url}>
         {label}
       </Link>
-      <MegaMenuPanel megaMenuType='mega_menu_1' />
+      <MegaMenuPanel megaMenuType='mega_menu_1' subMenuData={subMenuData} />
     </li>
   )
 }
