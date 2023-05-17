562085ebd0b247fb77c8b192cec309fd33254678
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-05-17 09:37:33 +0200

    removed ItemLink & working ref

--- src/components/structure/MainMenuItem/MainMenuItem.tsx
+++ src/components/structure/MainMenuItem/MainMenuItem.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Link from 'next/link';
 import MegaMenuPanel from '../MegaMenuPanel/MegaMenuPanel';
-
 interface MainMenuItemProps {
   label: string,
   url: string,
-  subMenuData?: any
+  subMenuData?: any,
+  hasChildren: boolean,
 }
 
 const MainMenuItem = ({ label, url, subMenuData }: MainMenuItemProps) => {
+  const ref = useRef(null);
+  const handleLinkClick = (event) => {
+    console.log(ref.current)
+  }
   return (
     <li>
-      <Link href={url}>
-        {label}
+      <Link href={url} legacyBehavior>
+        <a href="" ref={ref} onClick={handleLinkClick}>{label}</a>
       </Link>
       {subMenuData ? <MegaMenuPanel megaMenuType='mega_menu_1' subMenuData={subMenuData} /> : null}
-
     </li>
   )
 }
